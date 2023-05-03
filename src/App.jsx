387728f158769864f0c11560449f5636a201029c
ianbuen <ianbuen@gmail.com>
2023-05-03 12:03:05 +0800

    make time cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { HeaderCard } from './components/Card';
+import { HeaderCard, TimeCard } from './components/Card';
 import './styles/App.sass';
 import userImage from "./assets/image-jeremy.png";
 import { useState } from 'react';
+import data from "./data.json";
 
 function App() {
 
   const userName = "Jeremy Robson";
 
-  const state = useState(0);
+  const state = useState(1);
 
   return (
     <div className='App'>
       <HeaderCard img={userImage} name={userName} state={state} />
+
+      {data.map(({title, timeframes}, key) => 
+        <TimeCard key={key} title={title} timeframes={timeframes} state={state} />
+      )}
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,30 @@
 import "../styles/Card.sass"
 import PropTypes from 'prop-types';
+import ellipsis from "../assets/icon-ellipsis.svg";
 
-export const TimeCard = () => {
+export const TimeCard = ({title, timeframes, state}) => {
+
+  const [mode] = state;
+
+  const getTotalTime = () => {
+
+    switch (mode) {
+      case 0: return timeframes.daily;
+      case 1: return timeframes.weekly;
+      case 2: return timeframes.monthly;
+    }
+
+    return -1;
+  };
+
+  const {current, previous} = getTotalTime();
+
   return (
     <div className="TimeCard">
-
+      <h3>{title}</h3>
+      <img src={ellipsis} alt={'ellipsis'} />
+      <h4>{`${current}hrs`}</h4>
+      <p>{`Last week - ${previous}hrs`}</p>
     </div>
   )
 };
@@ -27,7 +47,14 @@ export const HeaderCard = ({img, name}) => {
   )
 }
 
+TimeCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  timeframes: PropTypes.object.isRequired,
+  state: PropTypes.object.isRequired
+};
+
 HeaderCard.propTypes = {
     img: PropTypes.object.isRequired,
-    name: PropTypes.string.isRequired
-}
+    name: PropTypes.string.isRequired,
+    state: PropTypes.object.isRequired
+};
